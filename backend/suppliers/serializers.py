0c7cdc2311d2a36d2e45fe7f98032d77fbe38bdf
Cody Hennighan <cody.hennighan@gmail.com>
2019-01-08 16:42:01 +0800

    Update serializers.py

--- backend/suppliers/serializers.py
+++ backend/suppliers/serializers.py
@@ -12,3 +12,10 @@ class SupplierSerializer(serializers.ModelSerializer):
     class Meta:
         model = Suppliers
         fields = ('supplierId', 'supplierName', 'supplierItems')
+        
+    def create(self, validated_data):
+        supplier_items_data = validated_data.pop('supplierItems')
+        supplier = Supplier.objects.create(**validated_data)
+        for supplier_item_data in supplier_items_data:
+            SupplierItems.objects.create(supplier=supplier, **supplier_item_data)
+        return supplier
