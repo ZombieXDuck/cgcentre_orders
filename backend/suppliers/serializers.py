51b7ccad3bde0e6a1ef8aa1c2194f6d927adfe35
Cody Hennighan <cody.hennighan@gmail.com>
2019-01-08 16:38:19 +0800

    Update serializers.py

--- backend/suppliers/serializers.py
+++ backend/suppliers/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from .models import Suppliers, SupplierItems
 
-class SupplierSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Suppliers
-        fields = ('supplierId', 'supplierName')
-
-class SupplierItemSerializer(serializers.ModelSerializer):
+class SupplierItemSerializer(serializers.ModelSerializer):    
     class Meta:
         model = SupplierItems
         fields = ('supplierId', 'supplierItemId', 'itemCode', 'itemName')
+        
+class SupplierSerializer(serializers.ModelSerializer):
+    supplierItems = SupplierItemSerializer(many=True)
+    
+    class Meta:
+        model = Suppliers
+        fields = ('supplierId', 'supplierName', 'supplierItems')
